825931298e94ad5dfeab8cc06ad26c38c6b5ec6a
LAMBOLEY <lucaslamboley@outlook.com>
2020-11-25 21:52:43 +0100

    Testing AceGUI library

--- Fourretout_Options/Core/Init.lua
+++ Fourretout_Options/Core/Init.lua
@@ -1,1 +1,11 @@
 local B, L, C, DB = unpack(Fourretout)
+
+local AceGUI = LibStub("AceGUI-3.0")
+
+local init = CreateFrame("Frame")
+init:RegisterEvent("PLAYER_LOGIN")
+init:SetScript("OnEvent", function()
+    local frame = AceGUI:Create("Frame")
+    frame:SetTitle("Example Frame")
+    frame:SetStatusText("AceGUI-3.0 Example Container Frame")
+end)
