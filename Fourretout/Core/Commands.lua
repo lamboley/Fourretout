f38c18f52118c2649952ee6c5ce49e6ab63a8697
LAMBOLEY <lucaslamboley@outlook.com>
2020-11-27 20:39:53 +0100

    Add command Get for Dev purpose

--- Fourretout/Core/Commands.lua
+++ Fourretout/Core/Commands.lua
@@ -1,5 +1,31 @@
 local E = unpack(select(2, ...))
 
+function E:GetPetGUID()
+    local summonedPetGUID = C_PetJournal.GetSummonedPetGUID()
+    if summonedPetGUID then
+        print(summonedPetGUID)
+    end
+end
+
+function E:GetMountID(spellID)
+    if spellID == nil then return end
+    local mountID = C_MountJournal.GetMountIDs()
+    for _, mountID in pairs(mountID) do
+        local mountName, mountSpellID = C_MountJournal.GetMountInfoByID(mountID)
+        if mountSpellID == tonumber(spellID) then
+            print(mountName..' '.. mountID)
+        end
+    end
+end
+
+function E:GetZoneID()
+    print(C_Map.GetBestMapForUnit('player'))
+end
+
 function E:LoadCommands()
+    self:RegisterChatCommand('petguid', 'GetPetGUID')
+    self:RegisterChatCommand('mountid', 'GetMountID')
+    self:RegisterChatCommand('zoneid', 'GetZoneID')
+
     self:RegisterChatCommand('frt', 'ToggleOptionsUI')
 end
