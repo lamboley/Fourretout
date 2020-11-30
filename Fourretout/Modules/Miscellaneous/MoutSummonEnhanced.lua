79483c66b48cc4b389e2b482f9f507315963edac
LAMBOLEY <lucaslamboley@outlook.com>
2020-11-30 10:51:29 +0100

    Fix MSE

--- Fourretout/Modules/Miscellaneous/MoutSummonEnhanced.lua
+++ Fourretout/Modules/Miscellaneous/MoutSummonEnhanced.lua
@@ -1,7 +1,7 @@
 local E, L = unpack(select(2, ...))
 local MSE = E:GetModule('MoutSummonEnhanced')
 
-local tonumber = tonumber
+local tonumber, tostring = tonumber, tostring
 
 local CanExitVehicle, VehicleExit, InCombatLockdown = CanExitVehicle, VehicleExit, InCombatLockdown
 local C_MountJournalSummonByID, C_MapGetBestMapForUnit = C_MountJournal.SummonByID, C_Map.GetBestMapForUnit
@@ -12,7 +12,7 @@ local mountHeirloom = {
     ['Horde'] = 678
 }
 
-function MSE:M()
+function MSE.M()
     if CanExitVehicle() then
         VehicleExit()
     elseif IsMounted() then
@@ -20,11 +20,11 @@ function MSE:M()
     elseif not InCombatLockdown() then
         if IsSpellKnown(33388) ~= true and IsSpellKnown(33391) ~= true then
                 if C_MapGetBestMapForUnit('player') == 378 then
-                    self.Print(L["You can't call heirloom mount since you haven't choosen a faction."])
+                    E.Print(L["You can't call heirloom mount since you haven't choosen a faction."])
                 else
                     C_MountJournalSummonByID(mountHeirloom[E.myFaction])
                 end
-        elseif (IsSpellKnown(34090) ~= true and IsSpellKnown(90266) ~= true) or (E.db.general.dontFly and E.db.general.dontFly[tostring(E:GetZoneID())]) or not IsFlyableArea() then
+        elseif (IsSpellKnown(34090) ~= true and IsSpellKnown(90266) ~= true) or (E.db.general.dontFly and E.db.general.dontFly[tostring(E.GetZoneID())]) or not IsFlyableArea() then
             C_MountJournalSummonByID(tonumber(E.db.mount.groundMount))
         else
             C_MountJournalSummonByID(tonumber(E.db.mount.flyingMount))
